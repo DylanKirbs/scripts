2566a27fd67b4fd46b8936bb2a2ea9291f14261d
Dylan Kirby <25853805@sun.ac.za>
2025-07-25 15:33:30 +0200

    Fix login bug (v2)

--- cs-stow.sh
+++ cs-stow.sh
@@ -9,7 +9,7 @@ VERSION="0.1.0"
 # --- Configurable Defaults --- #
 AUTO_CREATE_DIRS=(".config" ".local" ".vim" ".tmux" ".cache" ".mozilla")
 AUTO_CREATE_FILES=(".bashrc" ".vimrc" ".gitconfig" ".tmux.conf" ".profile")
-DEFAULT_IGNORES=(".git" ".Xauthority" ".Xsession*" ".profile" ".local/share/Trash" ".Trash*" "*.swp" "*.swo" ".*.swp" ".*.swo")
+DEFAULT_IGNORES=(".git" ".Xauthority" ".config" ".cache" ".dbus" ".pam_environment" ".bash_profile" ".bash_login" ".xinitrc" ".xsession" ".xsessionrc" ".local/share" ".Xsession*" ".profile" ".bash_profile" ".local/share/Trash" ".Trash*" "*.swp" "*.swo" ".*.swp" ".*.swo")
 IGNORE_FILES=(".$SCRIPT_NAME-ignore")
 
 # --- Runtime Flags --- #
